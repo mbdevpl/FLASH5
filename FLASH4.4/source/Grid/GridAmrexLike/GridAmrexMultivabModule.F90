b6091a304a13f5ea4ec30511170955e67f01e683
Klaus Weide <klaus@flash.uchicago.edu>
2017-03-15 21:41:21 -0500

    use Grid_blockMatch from Grid_blockMatch.F90

--- FLASH4.4/source/Grid/GridAmrexLike/GridAmrexMultivabModule.F90
+++ FLASH4.4/source/Grid/GridAmrexLike/GridAmrexMultivabModule.F90
@@ -1,78 +1,3 @@
-module Grid_extraInterface
-
-#include "constants.h"
-  implicit none
-contains
-  function Grid_blockMatch(blkID,ntype,refinementLevel) result(match)
-
-    use tree, ONLY : nodetype,lnblocks,neigh,lrefine
-    use Grid_data, ONLY : gr_oneBlock
-
-    logical               :: match
-    integer,intent(in   ) :: blkID
-    integer,intent(in   ) :: ntype
-    integer,intent(in   ),OPTIONAL :: refinementLevel
-
-    integer :: i
-    logical :: isBnd
-
-    i = blkID
-    if (i > lnblocks) then
-       match = .FALSE.
-       return                   !block number is not valid, return immediately!
-    end if
-
-    select case (ntype)
-    case (LEAF,PARENT_BLK,ANCESTOR)
-       match = (nodetype(i)==ntype)
-    case(ALL_BLKS)
-       match = .TRUE.
-    case (IBDRY_BLKS)
-       match = ((nodetype(i)==LEAF).or.(nodetype(i)==PARENT_BLK))
-       if (match) then
-          isBnd = (neigh(1,1,i) .LE. PARAMESH_PHYSICAL_BOUNDARY)
-          isBnd = isBnd.or.(neigh(1,2,i).LE.PARAMESH_PHYSICAL_BOUNDARY)
-          match = isBnd
-       end if
-    case (JBDRY_BLKS)
-       match = ((nodetype(i)==LEAF).or.(nodetype(i)==PARENT_BLK))
-       if (match) then
-          isBnd = (neigh(1,3,i) .LE. PARAMESH_PHYSICAL_BOUNDARY)
-          isBnd = isBnd.or.(neigh(1,4,i).LE.PARAMESH_PHYSICAL_BOUNDARY)
-          match = isBnd
-       end if
-    case(KBDRY_BLKS)
-       match = ((nodetype(i)==LEAF).or.(nodetype(i)==PARENT_BLK))
-       if (match) then
-          isBnd = (neigh(1,5,i) .LE. PARAMESH_PHYSICAL_BOUNDARY)
-          isBnd = isBnd.or.(neigh(1,6,i).LE.PARAMESH_PHYSICAL_BOUNDARY)
-          match = isBnd
-       end if
-    case(ANY_BDRY_BLKS)
-       match = ((nodetype(i)==LEAF).or.(nodetype(i)==PARENT_BLK))
-       if (match) then
-          match = ANY(neigh(1,1:6,i).LE.PARAMESH_PHYSICAL_BOUNDARY)
-       end if
-    case(ACTIVE_BLKS)
-       match = ((nodetype(i)==LEAF).or.(nodetype(i)==PARENT_BLK))
-    case(TRAVERSED)
-       match = (gr_oneBlock(i)%blockType==TRAVERSED) !DEV: What does this mean?
-    case(TRAVERSED_AND_ACTIVE)
-       match = (gr_oneBlock(i)%blockType==TRAVERSED_AND_ACTIVE) !DEV: What does this mean?
-    case(REFINEMENT)
-       if (present(refinementLevel)) then
-          match = (lrefine(i) == refinementLevel)
-       else
-          call Driver_abortFlash("[Grid_getListofBlocks] with ntype REFINEMENT optional argument refinementlevel must be present")
-       end if
-    case default
-       match = .FALSE.
-       call Driver_abortFlash("[Grid_blockMatch] ntype argument not recognized")
-    end select
-
-  end function Grid_blockMatch
-end module Grid_extraInterface
-
 module famrex_multivab_module
 
   use,intrinsic :: iso_c_binding
@@ -282,7 +207,7 @@ contains
   end subroutine famrex_mviter_clear
 
   logical function famrex_mviter_next (this)
-    use Grid_extraInterface,ONLY: Grid_blockMatch
+    use Grid_interface,ONLY: Grid_blockMatch
     class(famrex_mviter),intent(inout) :: this
     integer(c_int) :: isvalid
     logical        :: fisvalid
