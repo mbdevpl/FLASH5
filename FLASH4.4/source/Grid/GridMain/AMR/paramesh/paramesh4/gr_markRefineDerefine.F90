be27029a2d335cf5df82a1bb204cff2115e972eb
Anshu Dubey <adubey@lbl.gov>
2017-06-28 15:27:30 -0500

    making gr_markRefineDerefine less paramesh specific

--- FLASH4.4/source/Grid/GridMain/AMR/paramesh/paramesh4/gr_markRefineDerefine.F90
+++ FLASH4.4/source/Grid/GridMain/AMR/paramesh/paramesh4/gr_markRefineDerefine.F90
@@ -66,7 +66,8 @@ subroutine gr_markRefineDerefine(&
   real, intent(IN) :: refine_cutoff, derefine_cutoff, refine_filter
   integer, parameter :: SQNDIM = NDIM*NDIM
   
-  real delx,dely,delz
+  real,dimension(MDIM) ::  del, del_f
+  integer,dimension(MDIM) :: ncell
   real dely_f, delz_f
   real delu(mdim,il_bnd1:iu_bnd1,jl_bnd1:ju_bnd1,kl_bnd1:ku_bnd1)
   real delua(mdim,il_bnd1:iu_bnd1,jl_bnd1:ju_bnd1,kl_bnd1:ku_bnd1)
@@ -111,16 +112,6 @@ subroutine gr_markRefineDerefine(&
 ! this code to that situation would be uncommenting the following line:
 !!$  call Grid_fillGuardCells(CENTER_FACES,ALLDIR)
 
-! The following is unused code - it copies only the inner cells to work.
-!!$  do lb = 1,lnblocks
-!!$     do k = NGUARD*K3D+1,NGUARD*K3D+NZB
-!!$        do j = NGUARD*K2D+1,NGUARD*K2D+NYB
-!!$           do i = NGUARD+1,NGUARD+NXB
-!!$              work(i,j,k,lb,1) = unk(iref,i,j,k,lb)
-!!$           end do
-!!$        end do
-!!$     end do
-!!$  end do
 
 ! We are using more cell layers, including guardcells, from unk.
 
@@ -147,90 +138,62 @@ subroutine gr_markRefineDerefine(&
 
      if (nodetype(lb).eq.1.or.nodetype(lb).eq.2) then
 
-        if (no_permanent_guardcells) then
-           !just get the iref var:
-           gcell_on_cc = .false.
-           gcell_on_cc(iref) = .true.
-
-           idest = 1
-           iopt = 1
-           nlayers = NGUARD
-           lcc = .true.
-           lfc = .false.
-           lec = .false.
-           lnc = .false.
-           l_srl_only = .false.
-           icoord = 0
-           ldiag = .true.
-
-           call amr_1blk_guardcell(gr_meshMe,iopt,nlayers,lb,gr_meshMe, &
-                lcc,lfc,lec,lnc, &
-                l_srl_only,icoord,ldiag)
-
-           solnData => unk1(iref,:,:,:,idest)
-
-        else
-           solnData => unk(iref,:,:,:,lb)
-        end if
-
-
-        delx = 0.5e0*float(NXB)/bsize(1,lb)
+        solnData => unk(iref,:,:,:,lb)
         
-#if N_DIM >= 2
-        dely = 0.5e0*float(NYB)/bsize(2,lb)
-        dely_f = dely
-#endif
-        
-#if N_DIM == 3
-        delz = 0.5e0*float(NZB)/bsize(3,lb)
-        delz_f = delz
-#endif
+
+        del=0.0
+        ncell(IAXIS)=NXB
+        ncell(JAXIS)=NYB
+        ncell(KAXIS)=NZB
+
+        del(IAXIS:NDIM) = 0.5e0*float(ncell(IAXIS:NDIM))/bsize(IAXIS:NDIM,lb)
+        del_f(JAXIS:NDIM) = del(JAXIS:NDIM)
         
         ! Compute first derivatives
         
-        do k = 1+K3D,NZB+(K3D*((2*NGUARD)-1))
-           do j = 1+K2D,NYB+(K2D*((2*NGUARD)-1))
-              do i = 2,NXB+(2*NGUARD)-1
+        do k = 1+K3D,ncell(KAXIS)+(K3D*((2*NGUARD)-1))
+           do j = 1+K2D,ncell(JAXIS)+(K2D*((2*NGUARD)-1))
+              do i = 2,ncell(IAXIS)+(2*NGUARD)-1
                  
                  if (gr_geometry == SPHERICAL) &
-                      delx = 1.0/(XCOORD(i+1) - XCOORD(i-1))
-
+                      del(IAXIS) = 1.0/(XCOORD(i+1) - XCOORD(i-1))
+                 
                  ! d/dx
                  delu(1,i,j,k) = solnData(i+1,j,k) - solnData(i-1,j,k)
-                 delu(1,i,j,k) = delu(1,i,j,k)*delx
+                 delu(1,i,j,k) = delu(1,i,j,k)*del(IAXIS)
                  
                  delua(1,i,j,k) = abs(solnData(i+1,j,k)) + &
                       abs(solnData(i-1,j,k))
-                 delua(1,i,j,k) = delua(1,i,j,k)*delx
+                 delua(1,i,j,k) = delua(1,i,j,k)*del(IAXIS)
                  
 #if N_DIM >= 2
                  ! d/dy
                  if ((gr_geometry == SPHERICAL) .or. (gr_geometry == POLAR)) then
-                    dely_f = dely/XCOORD(i)
+                    del_f(JAXIS) = del(JAXIS)/XCOORD(i)
                  end if
 
                  delu(2,i,j,k) = solnData(i,j+1,k) - solnData(i,j-1,k)
-                 delu(2,i,j,k) = delu(2,i,j,k)*dely_f
+                 delu(2,i,j,k) = delu(2,i,j,k)*del_f(JAXIS)
                  
                  delua(2,i,j,k) = abs(solnData(i,j+1,k)) + &
                       abs(solnData(i,j-1,k))
-                 delua(2,i,j,k) = delua(2,i,j,k)*dely_f
+                 delua(2,i,j,k) = delua(2,i,j,k)*del_f(JAXIS)
 #endif
                  
 #if N_DIM == 3
                  ! d/dz
                  if (gr_geometry == SPHERICAL) then
-                    delz_f = delz/(  XCOORD(i) &
+                    del_f(KAXIS) = del(KAXIS)/(  XCOORD(i) &
                               &    * sin(YCOORD(j))  )
                  else if (gr_geometry == CYLINDRICAL) then
-                    delz_f = delz/XCOORD(i)
+                    del_f(KAXIS) = del(KAXIS)/XCOORD(i)
                  end if
                  delu(3,i,j,k) = solnData(i,j,k+1) -  solnData(i,j,k-1)
-                 delu(3,i,j,k) = delu(3,i,j,k)*delz_f
+                 delu(3,i,j,k) = delu(3,i,j,k)*del_f(KAXIS)
                  
                  delua(3,i,j,k) = abs(solnData(i,j,k+1)) + &
                       abs(solnData(i,j,k-1))
-                 delua(3,i,j,k) = delua(3,i,j,k)*delz_f
+                 delua(3,i,j,k) = delua(3,i,j,k)*del_f(KAXIS)
 #endif
                  
               end do
@@ -243,36 +206,36 @@ subroutine gr_markRefineDerefine(&
         
         ! Two guardcells
         kstart = 2*K3D+1
-        kend   = NZB+(K3D*((2*NGUARD)-2))
+        kend   = ncell(KAXIS)+(K3D*((2*NGUARD)-2))
         jstart = 2*K2D+1
-        jend   = NYB+(K2D*((2*NGUARD)-2))
+        jend   = ncell(JAXIS)+(K2D*((2*NGUARD)-2))
         istart = 3
-        iend   = NXB+(2*NGUARD)-2
+        iend   = ncell(IAXIS)+(2*NGUARD)-2
         ! One guardcell
         !            kstart = 2*K3D+1+K3D
-        !            kend   = NZB+(K3D*((2*NGUARD)-2))-K3D
+        !            kend   = ncell(KAXIS)+(K3D*((2*NGUARD)-2))-K3D
         !            jstart = 2*K2D+1+K2D
-        !            jend   = NYB+(K2D*((2*NGUARD)-2))-K2D
+        !            jend   = ncell(JAXIS)+(K2D*((2*NGUARD)-2))-K2D
         !            istart = NGUARD
-        !            iend   = NXB+(2*NGUARD)-3
+        !            iend   = ncell(IAXIS)+(2*NGUARD)-3
         ! No guardcells
         !            kstart = K3D*NGUARD+1
-        !            kend   = NZB+K3D*NGUARD
+        !            kend   = ncell(KAXIS)+K3D*NGUARD
         !            jstart = K2D*NGUARD+1
-        !            jend   = NYB+K2D*NGUARD
+        !            jend   = ncell(JAXIS)+K2D*NGUARD
         !            istart = NGUARD+1
-        !            iend   = NXB+NGUARD
+        !            iend   = ncell(IAXIS)+NGUARD
         
         if (neigh(1,1,lb).le.-20) istart = NGUARD+1
-        if (neigh(1,2,lb).le.-20) iend   = NGUARD+NXB
+        if (neigh(1,2,lb).le.-20) iend   = NGUARD+ncell(IAXIS)
         
 #if N_DIM >= 2
         if (neigh(1,3,lb).le.-20) jstart = NGUARD*K2D+1
-        if (neigh(1,4,lb).le.-20) jend   = NGUARD*K2D+NYB
+        if (neigh(1,4,lb).le.-20) jend   = NGUARD*K2D+ncell(JAXIS)
 #endif
 #if N_DIM == 3
         if (neigh(1,5,lb).le.-20) kstart = NGUARD*K3D+1
-        if (neigh(1,6,lb).le.-20) kend   = NGUARD*K3D+NZB
+        if (neigh(1,6,lb).le.-20) kend   = NGUARD*K3D+ncell(KAXIS)
 #endif
         
         do k = kstart,kend
@@ -280,112 +243,112 @@ subroutine gr_markRefineDerefine(&
               do i = istart,iend
                  
                  if (gr_geometry == SPHERICAL) &
-                      delx = 1.0/(XCOORD(i+1) - XCOORD(i-1))
+                      del(IAXIS) = 1.0/(XCOORD(i+1) - XCOORD(i-1))
 
                  ! d/dxdx
                  delu2(1) = delu(1,i+1,j,k) - delu(1,i-1,j,k)
-                 delu2(1) = delu2(1)*delx
+                 delu2(1) = delu2(1)*del(IAXIS)
                  
                  delu3(1) = abs(delu(1,i+1,j,k)) + abs(delu(1,i-1,j,k))
-                 delu3(1) = delu3(1)*delx
+                 delu3(1) = delu3(1)*del(IAXIS)
                  
                  delu4(1) = delua(1,i+1,j,k) + delua(1,i-1,j,k)
-                 delu4(1) = delu4(1)*delx
+                 delu4(1) = delu4(1)*del(IAXIS)
                  
 #if N_DIM >= 2
                  if ((gr_geometry == SPHERICAL) .or. (gr_geometry == POLAR)) then
-                    dely_f = dely/XCOORD(i)
+                    del_f(JAXIS) = del(JAXIS)/XCOORD(i)
                  end if
 
                  ! d/dydx
                  delu2(2) = delu(1,i,j+1,k) - delu(1,i,j-1,k)
-                 delu2(2) = delu2(2)*dely_f
+                 delu2(2) = delu2(2)*del_f(JAXIS)
                  
                  delu3(2) = abs(delu(1,i,j+1,k)) + abs(delu(1,i,j-1,k))
-                 delu3(2) = delu3(2)*dely_f
+                 delu3(2) = delu3(2)*del_f(JAXIS)
                  
                  delu4(2) = delua(1,i,j+1,k) + delua(1,i,j-1,k)
-                 delu4(2) = delu4(2)*dely_f
+                 delu4(2) = delu4(2)*del_f(JAXIS)
                  
                  ! d/dxdy
                  delu2(3) = delu(2,i+1,j,k) - delu(2,i-1,j,k)
-                 delu2(3) = delu2(3)*delx
+                 delu2(3) = delu2(3)*del(IAXIS)
                  
                  delu3(3) = abs(delu(2,i+1,j,k)) + abs(delu(2,i-1,j,k))
-                 delu3(3) = delu3(3)*delx
+                 delu3(3) = delu3(3)*del(IAXIS)
                  
                  delu4(3) = delua(2,i+1,j,k) + delua(2,i-1,j,k)
-                 delu4(3) = delu4(3)*delx
+                 delu4(3) = delu4(3)*del(IAXIS)
                  
                  ! d/dydy
                  delu2(4) = delu(2,i,j+1,k) - delu(2,i,j-1,k)
-                 delu2(4) = delu2(4)*dely_f
+                 delu2(4) = delu2(4)*del_f(JAXIS)
                  
                  delu3(4) = abs(delu(2,i,j+1,k)) +  &
                       &                          abs(delu(2,i,j-1,k))
-                 delu3(4) = delu3(4)*dely_f
+                 delu3(4) = delu3(4)*del_f(JAXIS)
                  
                  delu4(4) = delua(2,i,j+1,k) + delua(2,i,j-1,k)
-                 delu4(4) = delu4(4)*dely_f
+                 delu4(4) = delu4(4)*del_f(JAXIS)
 #endif
                  
 #if N_DIM == 3
                  if (gr_geometry == SPHERICAL) then
-                    delz_f = delz/(  XCOORD(i) &
+                    del_f(KAXIS) = del(KAXIS)/(  XCOORD(i) &
                               &    * sin(YCOORD(j))  )
                  else if (gr_geometry == CYLINDRICAL) then
-                    delz_f = delz/XCOORD(i)
+                    del_f(KAXIS) = del(KAXIS)/XCOORD(i)
                  end if
 
                  ! d/dzdx
                  delu2(5) = delu(1,i,j,k+1) - delu(1,i,j,k-1)
-                 delu2(5) = delu2(5)*delz_f
+                 delu2(5) = delu2(5)*del_f(KAXIS)
                  
                  delu3(5) = abs(delu(1,i,j,k+1)) + abs(delu(1,i,j,k-1))
-                 delu3(5) = delu3(5)*delz_f
+                 delu3(5) = delu3(5)*del_f(KAXIS)
                  
                  delu4(5) = delua(1,i,j,k+1) + delua(1,i,j,k-1)
-                 delu4(5) = delu4(5)*delz_f
+                 delu4(5) = delu4(5)*del_f(KAXIS)
                  
                  ! d/dzdy
                  delu2(6) = delu(2,i,j,k+1) - delu(2,i,j,k-1)
-                 delu2(6) = delu2(6)*delz_f
+                 delu2(6) = delu2(6)*del_f(KAXIS)
                  
                  delu3(6) = abs(delu(2,i,j,k+1)) + abs(delu(2,i,j,k-1))
-                 delu3(6) = delu3(6)*delz_f
+                 delu3(6) = delu3(6)*del_f(KAXIS)
                  
                  delu4(6) = delua(2,i,j,k+1) + delua(2,i,j,k-1)
-                 delu4(6) = delu4(6)*delz_f
+                 delu4(6) = delu4(6)*del_f(KAXIS)
                  
                  ! d/dxdz
                  delu2(7) = delu(3,i+1,j,k) - delu(3,i-1,j,k)
-                 delu2(7) = delu2(7)*delx
+                 delu2(7) = delu2(7)*del(IAXIS)
                  
                  delu3(7) = abs(delu(3,i+1,j,k)) + abs(delu(3,i-1,j,k))
-                 delu3(7) = delu3(7)*delx
+                 delu3(7) = delu3(7)*del(IAXIS)
                  
                  delu4(7) = delua(3,i+1,j,k) + delua(3,i-1,j,k)
-                 delu4(7) = delu4(7)*delx
+                 delu4(7) = delu4(7)*del(IAXIS)
                  
                  ! d/dydz
                  delu2(8) = delu(3,i,j+1,k) - delu(3,i,j-1,k)
-                 delu2(8) = delu2(8)*dely_f
+                 delu2(8) = delu2(8)*del_f(JAXIS)
                  
                  delu3(8) = abs(delu(3,i,j+1,k)) + abs(delu(3,i,j-1,k))
-                 delu3(8) = delu3(8)*dely_f
+                 delu3(8) = delu3(8)*del_f(JAXIS)
                  
                  delu4(8) = delua(3,i,j+1,k) + delua(3,i,j-1,k)
-                 delu4(8) = delu4(8)*dely_f
+                 delu4(8) = delu4(8)*del_f(JAXIS)
                  
                  ! d/dzdz
                  delu2(9) = delu(3,i,j,k+1) - delu(3,i,j,k-1)
-                 delu2(9) = delu2(9)*delz_f
+                 delu2(9) = delu2(9)*del_f(KAXIS)
                  
                  delu3(9) = abs(delu(3,i,j,k+1)) + abs(delu(3,i,j,k-1))
-                 delu3(9) = delu3(9)*delz_f
+                 delu3(9) = delu3(9)*del_f(KAXIS)
                  
                  delu4(9) = delua(3,i,j,k+1) + delua(3,i,j,k-1)
-                 delu4(9) = delu4(9)*delz_f
+                 delu4(9) = delu4(9)*del_f(KAXIS)
 #endif
 
 ! compute the error
