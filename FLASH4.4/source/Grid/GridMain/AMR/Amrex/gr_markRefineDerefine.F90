76cfbde7f54adcb772eb09e2b91000b4983a2bc7
Klaus Weide <klaus@flash.uchicago.edu>
2017-09-14 14:35:56 -0500

    add a few lines from the User's Guide, quite pointless so far

--- FLASH4.4/source/Grid/GridMain/AMR/Amrex/gr_markRefineDerefine.F90
+++ FLASH4.4/source/Grid/GridMain/AMR/Amrex/gr_markRefineDerefine.F90
@@ -1,6 +1,7 @@
 subroutine gr_markRefineDerefine(lev, tags, time, tagval, clearval) bind(c)
    use iso_c_binding
    use amrex_fort_module, ONLY : wp => amrex_real
+   use amrex_tagbox_module, ONLY : amrex_tagboxarray
    
    implicit none
    
@@ -9,6 +10,10 @@ subroutine gr_markRefineDerefine(lev, tags, time, tagval, clearval) bind(c)
    real(wp),          intent(in), value :: time
    character(c_char), intent(in), value :: tagval
    character(c_char), intent(in), value :: clearval
+
+   type ( amrex_tagboxarray ) :: tag
+
+   tag = tags
 
    write(*,*) "[gr_markRefineDerefine]"
 end subroutine gr_markRefineDerefine
