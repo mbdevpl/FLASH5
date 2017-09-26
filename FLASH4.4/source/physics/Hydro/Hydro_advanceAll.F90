4d023a8dcde3929f082d5fadf97e69e7fdaa72c3
Jared O'Neal <joneal@anl.gov>
2017-09-25 19:32:31 -0500

    Create some Hydro stubs

---
+++ FLASH4.4/source/physics/Hydro/Hydro_advanceAll.F90
@@ -0,0 +1,10 @@
+#include "constants.h"
+
+
+subroutine Hydro_advanceAll(simTime, dt, dtOld)
+  implicit none
+
+  real, intent(IN) ::  simTime, dt, dtOld
+
+  return
+end subroutine Hydro_advanceAll
