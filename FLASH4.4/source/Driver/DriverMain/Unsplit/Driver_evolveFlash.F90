e7eb3d2876ce7ebec857efd86e6af1c9aac058cd
Klaus Weide <klaus@flash.uchicago.edu>
2017-09-28 11:14:47 -0500

    undo removal of source/Driver/DriverMain/Unsplit/Driver_evolveFlash.F90

---
+++ FLASH4.4/source/Driver/DriverMain/Unsplit/Driver_evolveFlash.F90
@@ -0,0 +1,416 @@
+!!****if* source/Driver/DriverMain/Unsplit/Driver_evolveFlash
+!!
+!! NAME
+!!
+!!  Driver_evolveFlash
+!!
+!! SYNOPSIS
+!!
+!!  call Driver_evolveFlash()
+!!
+!! DESCRIPTION
+!!
+!! This routine implements a directionally unsplit integrator for time 
+!! advancement. A single step in this driver accounts for all 
+!! multidimensional fluxes and updates conserved variables for the next
+!! time step. The routine is a default driver for the Unsplit Staggered
+!! Mesh (USM) MHD and Unsplit Hydro solvers.
+!!
+!! NOTES
+!!
+!! The Driver unit uses a few unit scope variables that are
+!! accessible to all routines within the unit, but not to the
+!! routines outside the unit. These variables begin with "dr_",
+!! like dr_globalMe, dr_dt, or dr_beginStep, and are stored in Fortran
+!! module Driver_data (in file Driver_data.F90). The other variables
+!! are local to the specific routine and do not have the prefix "dr_".
+!!
+!!
+!!***
+
+!#define DEBUG_DRIVER
+#ifdef DEBUG_ALL
+#define DEBUG_DRIVER
+#endif
+
+
+subroutine Driver_evolveFlash()
+
+  use Driver_data,         ONLY : dr_globalMe, dr_globalNumProcs, dr_nbegin, &
+                                  dr_meshMe, dr_meshNumProcs,            &
+                                  dr_nend, dr_dt,                        &
+                                  dr_tmax, dr_simTime, dr_redshift,      &
+                                  dr_nstep, dr_dtOld, dr_dtNew,          &
+                                  dr_simGeneration,                      &
+                                  dr_restart,                            &
+                                  dr_redshiftOld, dr_useRedshift,        &
+                                  dr_redshiftfinal,                      &
+                                  dr_useSTS, dr_nuSTS, dr_nstepTotalSTS, &
+                                  dr_dtSTS,dr_dt_subSTS,                 &
+                                  dr_dtAdvect, dr_dtDiffuse,             &
+                                  dr_useSTSforDiffusion,                 &
+                                  dr_tstepChangeFactor,                  &
+                                  dr_allowDtSTSDominate,dr_meshComm
+  use Driver_interface,    ONLY : Driver_sourceTerms, Driver_computeDt, &
+                                  Driver_superTimeStep, &
+                                  Driver_logMemoryUsage, &
+                                  Driver_driftUnk, &
+                                  Driver_diagnostics
+  use Logfile_interface,   ONLY : Logfile_stamp, Logfile_close
+  use Timers_interface,    ONLY : Timers_start, Timers_stop, &
+                                  Timers_getSummary
+  use Diffuse_interface,   ONLY : Diffuse
+  use Particles_interface, ONLY : Particles_advance, Particles_dump
+  use Grid_interface,      ONLY : Grid_getLocalNumBlks, &
+                                  Grid_getListOfBlocks, &
+                                  Grid_getBlkIndexLimits, &
+                                  Grid_updateRefinement,&
+                                  Grid_fillGuardCells,&
+                                  Grid_getDeltas,&
+                                  Grid_getBlkPtr,&
+                                  Grid_releaseBlkPtr,&
+                                  Grid_getMaxRefinement
+  use Grid_interface,      ONLY : Grid_copyF4DataToMultiFabs
+#ifdef FLASH_GRID_AMREX
+  ! DEV: Temporary ugliness for debugging
+  use amrex_interfaces,    ONLY : gr_writeData
+#endif
+
+#include "Flash.h"
+#ifdef FLASH_GRID_AMREXTRANSITION
+  use gr_amrextInterface,  ONLY : gr_amrextBuildMultiFabsFromF4Grid
+  use gr_amrextData
+#endif
+  use Hydro_interface,     ONLY : Hydro_advanceAll, &
+                                  Hydro_gravPotIsAlreadyUpdated
+  use Gravity_interface,   ONLY : Gravity_potentialListOfBlocks
+  use IO_interface,        ONLY : IO_output,IO_outputFinal
+  use RadTrans_interface,  ONLY : RadTrans
+  use Eos_interface,       ONLY : Eos_logDiagnostics
+  use Simulation_interface, ONLY: Simulation_adjustEvolution
+  use Profiler_interface, ONLY : Profiler_start, Profiler_stop
+  use block_iterator, ONLY : block_iterator_t
+  use block_metadata, ONLY : block_metadata_t
+
+#ifdef FLASH_GRID_AMREXTRANSITION
+!!$  use amrex_amr_module,    ONLY : amrex_real
+!!$  use amrex_box_module,    ONLY : amrex_box
+!  use amrex_box_module
+!  use amrex_fab_module
+  use amrex_multifab_module
+#else
+  use Driver_data, ONLY : gr_amrextUnkMFs => dr_simGeneration
+#endif
+
+  implicit none
+
+#include "constants.h"
+
+  integer   :: localNumBlocks
+
+  integer,save :: sweepDummy = SWEEP_ALL
+
+  ! for logfile output
+  character(len=MAX_STRING_LENGTH), dimension(4,2) :: strBuff
+  character(len=15) :: numToStr
+
+  logical :: gridChanged
+  logical :: endRunPl !Should we end our run on this iteration, based on conditions detected by the IO unit?
+  logical :: endRun !Should we end our run on this iteration, based on conditions detected by the IO unit?
+  logical :: endRunWallClock !Should we end our run on this iteration, based on wall clock time?
+
+  ! for super-time-stepping
+  integer :: nstepSTS
+  real    :: dt_diffuse_temp
+  real    :: dtNewTemp
+  logical :: useSTS_local
+  integer :: nstepTotalSTS_local 
+
+  integer, parameter :: driftUnk_flags = DRIFT_NO_PARENTS
+#ifdef DEBUG_GRID_GCMASK
+  logical,save :: gcMaskLogged =.FALSE.
+#else
+  logical,save :: gcMaskLogged =.TRUE.
+#endif
+  integer, dimension(LOW:HIGH,MDIM) :: blkLimits,blkLimitsGC
+  integer :: blockCount
+  integer,dimension(MAXBLOCKS)::blks
+  real,pointer,dimension(:,:,:,:) :: Uout, Uin
+  real,dimension(MDIM) :: del
+
+#ifdef FLASH_GRID_AMREXTRANSITION
+  type(amrex_multifab),allocatable :: phi_mf(:)
+#endif
+  integer:: ib, blockID, level, maxLev
+
+  type(block_iterator_t) :: itor
+  type(block_metadata_t) :: block
+
+!!$  real(amrex_real)  :: time     !testing...
+  logical :: nodal(3)
+!!$  type(amrex_multifab), allocatable :: phiborder(:)
+!  type(amrex_octree_iter) :: oti
+!!$  type(amrex_box) :: abx, atbx
+!!$  real(amrex_real), contiguous, pointer, dimension(:,:,:,:) :: pin,pout,pux,puy,puz,pfx,pfy,pfz, &
+!!$       pf, pfab
+!!$  type(amrex_fab) :: uface(NDIM)
+!!$  type(amrex_multifab), allocatable :: fluxes(:,:)
+
+
+  endRunPl = .false.
+  endRun = .false.
+
+  call Logfile_stamp( 'Entering evolution loop' , '[Driver_evolveFlash]')
+  call Profiler_start("FLASH_evolution")
+  call Timers_start("evolution")
+
+  call Grid_getMaxRefinement(maxLev,mode=1) !mode=1 means lrefine_max, which does not change during sim.
+
+  do dr_nstep = dr_nBegin, dr_nend
+     
+     useSTS_local = dr_useSTS
+
+     if (dr_globalMe == MASTER_PE) then
+        
+        write (numToStr(1:), '(I10)') dr_nstep
+        write (strBuff(1,1), "(A)") "n"
+        write (strBuff(1,2), "(A)") trim(adjustl(numToStr))
+        
+        write (numToStr(1:), "(1PE12.6)") dr_simTime
+        write (strBuff(2,1), "(A)") "t"
+        write (strBuff(2,2), "(A)") trim(adjustl(numToStr))
+        
+        if (.not. dr_useSTS) then
+           write (numToStr(1:), "(1PE12.6)") dr_dt
+        else
+           write (numToStr(1:), "(1PE12.6)") max(dr_dt,dr_dtSTS)
+        endif
+        write (strBuff(3,1), "(A)") "dt"
+        write (strBuff(3,2), "(A)") trim(adjustl(NumToStr))
+        
+        call Logfile_stamp( strBuff(1:3,:), 3, 2, "step")
+        
+        
+     end if
+     
+!!     call Simulation_adjustEvolution(blockCount, blockList, dr_nstep, dr_dt, dr_simTime)
+     
+     call Driver_driftUnk(__FILE__,__LINE__,driftUnk_flags)
+     
+     dr_simTime = dr_simTime + dr_dt
+     dr_simGeneration = 0
+     
+     ! 2. Hydro/MHD
+#ifdef DEBUG_DRIVER
+     print*,'going into Hydro/MHD'  ! DEBUG
+     print*,'going into hydro myPE=',dr_globalMe
+#endif
+     !!ChageForAMRex -- Here is where we put in the iterator and extract the relevant metadata
+     !!ChageForAMRex -- from the iterator and then use the case statement to transfer control to the
+     !!ChageForAMRex -- right implementation.
+     
+#ifdef DEBUG_GRID_GCMASK
+     if (.NOT.gcMaskLogged) then
+        !!        call Logfile_stampVarMask(hy_gcMask, .FALSE., '[hy_hllUnsplit]', 'gcNeed')
+     end if
+#endif
+     
+     !! Guardcell filling routine
+!!$     call Grid_fillGuardCells(CENTER,ALLDIR,&
+!!$          maskSize=hy_gcMaskSize, mask=hy_gcMask,makeMaskConsistent=.true.,doLogMask=.NOT.gcMaskLogged)
+     
+     call Grid_fillGuardCells(CENTER,ALLDIR)
+
+     call Hydro_advanceAll(dr_simTime, dr_dt, dr_dtOld)
+#if(0)
+!!!!!! MOVED TO Hydro_advanceAll !!!!!!
+!vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+     call Timers_start("Hydro")
+
+#ifdef FLASH_GRID_AMREXTRANSITION
+     allocate(phi_mf(maxLev))
+#endif
+!!$     call gr_amrextBuildMultiFabsFromF4Grid(phi_mf, maxLev, LEAF)
+#ifdef FLASH_GRID_AMREXTRANSITION
+     call gr_amrextBuildMultiFabsFromF4Grid(gr_amrextUnkMFs, maxLev, LEAF)
+#endif
+     call Grid_copyF4DataToMultiFabs(CENTER, nodetype=LEAF)
+
+     do level=1,maxLev
+#ifdef DEBUG_DRIVER
+        print*,' ***************   HYDRO LEVEL', level,'  **********************'
+#endif
+
+        itor = block_iterator_t(LEAF, level=level)
+        do while(itor%is_valid())
+           call itor%blkMetaData(block)
+
+           blkLimits(:,:)   = block%localLimits
+           blkLimitsGC(:,:) = block%localLimitsGC
+           
+           call Grid_getBlkPtr(block, Uout,localFlag=.TRUE.)
+!!$           abx = amrex_box(bx%lo, bx%hi, bx%nodal)
+!!$           call amrex_print(abx)
+!!$           tbx = abx
+
+           call Grid_getDeltas(level,del)
+           Uin => Uout
+           call Hydro(block,blkLimitsGC,Uin, blkLimits, Uout, del,dr_simTime, dr_dt, dr_dtOld,  sweepDummy)
+           call Grid_releaseBlkPtr(block, Uout)
+           nullify(Uout)
+ 
+           call itor%next()
+        end do
+        call Timers_stop("Hydro")
+#ifdef DEBUG_DRIVER
+        print*, 'return from Hydro/MHD timestep'  ! DEBUG
+        print*,'returning from hydro myPE=',dr_globalMe
+#endif
+        
+        
+!!$     ! 8. Diagnostics
+!!$     call Timers_start("diagnostics")
+!!$     call Driver_diagnostics(blockCount, blockList, dr_dt)
+!!$     call Timers_stop("diagnostics")
+!!$#ifdef DEBUG_DRIVER
+!!$     print*, 'return from Diagnostics '  ! DEBUG
+!!$#endif
+        
+        !! save for old dt
+     end do
+#ifdef FLASH_GRID_AMREXTRANSITION
+     do level=1,maxLev
+        call amrex_multifab_destroy(phi_mf(level))
+     end do
+     deallocate(phi_mf)
+#endif
+
+!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+!!!!!! MOVED TO Hydro_advanceAll !!!!!!
+#endif
+
+
+     call Grid_copyF4DataToMultiFabs(CENTER, nodetype=LEAF, reverse=.TRUE.)
+#ifdef FLASH_GRID_AMREXTRANSITION
+     call gr_amrextBuildMultiFabsFromF4Grid(gr_amrextUnkMFs, maxLev, ACTIVE_BLKS)
+#endif
+     call Grid_copyF4DataToMultiFabs(CENTER, nodetype=ACTIVE_BLKS)
+
+     dr_dtOld = dr_dt
+
+     ! DEV: TODO Can we build this into calls already in this file?
+     call Grid_averageDownLevels
+
+     !----
+     !- End Physics Sequence
+     !--------------------------------------------------------------------
+     
+     !output a plotfile before the grid changes
+     call Timers_start("IO_output")
+     
+     if (.not. useSTS_local) then
+        call IO_output(dr_simTime, &
+             dr_dt, dr_nstep+1, dr_nbegin, endRunPl, PLOTFILE_AND_PARTICLEFILE)
+     else
+        call IO_output(dr_simTime, &
+             dr_dtSTS, dr_nstep+1, dr_nbegin, endRunPl, PLOTFILE_AND_PARTICLEFILE)
+     endif
+
+#ifdef FLASH_GRID_AMREX
+     ! DEV: Temporary ugliness for debugging
+     call gr_writeData(dr_nstep, dr_simTime)
+#endif
+     call Timers_stop("IO_output")
+     
+     
+     call Timers_start("Grid_updateRefinement")
+     call Grid_updateRefinement(dr_nstep, dr_simTime, gridChanged)
+     call Timers_stop("Grid_updateRefinement")
+     
+     if (gridChanged) dr_simGeneration = dr_simGeneration + 1
+     
+     ! backup needed old
+     if (.not. useSTS_local) dr_dtOld = dr_dt
+     
+#ifdef DEBUG_DRIVER
+     print*, 'going into Driver_computeDt '  ! DEBUG
+#endif
+     ! calculate new
+     call Timers_start("Driver_computeDt")
+     call Driver_computeDt(dr_nbegin,  dr_nstep,      &
+          dr_simTime, dr_dtOld, dr_dtNew)
+     call Timers_stop("Driver_computeDt")
+#ifdef DEBUG_DRIVER
+     print*, 'return from Driver_computeDt '  ! DEBUG
+#endif
+     
+     ! store new
+     if (.not. useSTS_local) dr_dt = dr_dtNew
+     
+     call Timers_start("IO_output")
+     if (.not. useSTS_local) then
+        call IO_output(dr_simTime,dr_dt,dr_nstep+1,dr_nbegin,endRun,&
+             CHECKPOINT_FILE_ONLY)
+     else
+        call IO_output(dr_simTime,dr_dtSTS,dr_nstep+1,dr_nbegin,endRun,&
+             CHECKPOINT_FILE_ONLY)
+     endif
+     call Timers_stop("IO_output")
+     endRun = (endRunPl .OR. endRun)
+     
+     call Eos_logDiagnostics(.FALSE.)
+     
+     
+     !!*****************************************************************************
+     !!  Evolution Loop -- check termination conditions
+     !!*****************************************************************************
+     
+     !Exit if a .dump_restart or .kill was found during the last step
+     if(endRun) exit
+     
+     !! the simulation ends before nend iterations if
+     !!  (i)   the simulation time is greater than the maximum time (tmax)
+     !!  (ii)  the redshift falls below the minimum redshift  
+     !!        (also called redshiftFinal) 
+     !!  (iii) the wall clock time is greater than the maximum 
+     !!        (wall_clock_time_max)
+     
+     
+     if (dr_simTime >= dr_tmax) then
+        if(dr_globalMe == MASTER_PE) then
+           print *, "exiting: reached max SimTime"
+        endif
+        exit
+     end if
+     
+     call dr_wallClockLimitExceeded(endRunWallClock)
+     if (endRunWallClock) then
+        if(dr_globalMe == MASTER_PE) then
+           print *, "exiting: reached max wall clock time"
+        endif
+        exit
+     end if
+     
+  enddo
+  !The value of dr_nstep after the loop is (dr_nend + 1) if the loop iterated for
+  !the maximum number of times.  However, we need to retain the value that
+  !dr_nstep had during the last loop iteration, otherwise the number for nstep
+  !that will be stored in a final checkpoint file will be wrong.
+  dr_nstep = min(dr_nstep,dr_nend)
+  
+  !!******************************************************************************
+  !! End of Evolution Loop
+  !!******************************************************************************
+  
+  call Timers_stop("evolution")
+  call Profiler_stop("FLASH_evolution")
+  call Logfile_stamp( 'Exiting evolution loop' , '[Driver_evolveFlash]')
+  !if a file termination, this may already be done.
+  if(.NOT.endRun) call IO_outputFinal()
+  call Timers_getSummary( max(0,dr_nstep-dr_nbegin+1))
+  call Logfile_stamp( "FLASH run complete.", "LOGFILE_END")
+  call Logfile_close()
+  
+  return
+  
+end subroutine Driver_evolveFlash
