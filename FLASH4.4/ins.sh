864058942813fc2d19477b44b0266759bf092c7c
Akash V Dhruv <adhruv@pfe27.nas.nasa.gov>
2018-06-28 13:10:10 -0700

    removing ins and sodtest setup files from main directory

--- FLASH4.4/ins.sh
+++
@@ -1,1 +0,0 @@
-./setup unitTest/INS_amrex -auto -2d -nxb=16 -nyb=16 -index-reorder -unit=Grid/GridMain/AMR/Amrex +serialio -unit=IO/IOMain/hdf5/serial/AM -maxblocks=16 -site=pfe.nas.nasa.gov -objdir=INS_amrex
