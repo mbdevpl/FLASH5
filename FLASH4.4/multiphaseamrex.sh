cf5b7ebaefda617d882a5ec1f680d4f037f8b074
akashdhruv <akashdhruv@gwmail.gwu.edu>
2018-06-28 11:57:37 -0400

    setting up cases for INS

--- FLASH4.4/multiphaseamrex.sh
+++
@@ -1,1 +0,0 @@
-./setup unitTest/Multiphase_Amrex -auto -3d -nxb=16 -nyb=16 -nzb=16 -index-reorder -unit=Grid/GridMain/AMR/Amrex +serialio -unit=IO/IOMain/hdf5/serial/AM -maxblocks=16 -site=pfe.nas.nasa.gov -objdir=multiphaseamrex
--- FLASH4.4/source/Simulation/SimulationMain/unitTest/Multiphase_Amrex/multiphaseamrex.sh
+++
@@ -1,1 +0,0 @@
-./setup unitTest/Multiphase_Amrex -auto -2d -nxb=16 -nyb=16 -unit=Grid/GridMain/AMR/Amrex +serialio -unit=IO/IOMain/hdf5/serial/AM -maxblocks=16 -site=pfe.nas.nasa.gov -objdir=multiphaseamrex
