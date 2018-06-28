ab27a4c4fe57d007ebf2eee183292f9b8ebc33e2
Saurabh Chawdhary <saurabh.chawdhary@gmail.com>
2018-06-28 17:42:44 -0500

    adding index-reorder and move Grid?AMR?Amrex to Config

--- FLASH4.4/source/Simulation/SimulationMain/unitTest/Multigrid_Amrex/setup.sh
+++ FLASH4.4/source/Simulation/SimulationMain/unitTest/Multigrid_Amrex/setup.sh
@@ -1,1 +1,1 @@
-./setup unitTest/Multigrid_Amrex -auto -3d -unit=Grid/GridMain/AMR/Amrex +serialio -unit=IO/IOMain/hdf5/serial/AM -maxblocks=1000 -site=sauc.xps -objdir=multigridamrex
+./setup unitTest/Multigrid_Amrex -auto -3d  +serialio -unit=IO/IOMain/hdf5/serial/AM -index-reorder -maxblocks=1000 -site=sauc.xps -objdir=multigridamrex
