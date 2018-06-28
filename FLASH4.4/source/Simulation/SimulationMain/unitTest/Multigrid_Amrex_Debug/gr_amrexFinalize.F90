38a0f93b059948a0668518f44dc0d152619fe0a4
Akash V Dhruv <adhruv@pfe27.nas.nasa.gov>
2018-06-28 14:31:42 -0700

    Adding Multigrid_Amrex_Debug unit test for experimentation

---
+++ FLASH4.4/source/Simulation/SimulationMain/unitTest/Multigrid_Amrex_Debug/gr_amrexFinalize.F90
@@ -0,0 +1,55 @@
+!!****if* source/Grid/GridMain/AMR/Amrex/gr_amrexFinalize
+!!
+!! NAME
+!!  gr_amrexFinalize
+!!
+!! SYNOPSIS
+!!  gr_amrexFinalize
+!!           
+!! DESCRIPTION
+!!  Clean-up all data structures managed by the Grid unit and allow AMReX to
+!!  clean-up and terminate octree-based operations.
+!!
+!!***
+
+subroutine gr_amrexFinalize()
+    use iso_c_binding
+    use amrex_init_module,         ONLY : amrex_finalize
+    use amrex_amrcore_module,      ONLY : amrex_max_level, &
+                                          amrex_amrcore_finalize
+    use amrex_octree_module,       ONLY : amrex_octree_finalize
+
+    use gr_amrexInterface,         ONLY : gr_clearLevelCallback
+    use gr_physicalMultifabs,      ONLY : unk, &
+                                          gr_scratchCtr, &
+                                          facevarx, facevary, facevarz, &
+                                          fluxes, &
+                                          flux_registers
+
+    use Driver_data,               ONLY: dr_globalMe
+
+#include"constants.h"
+ 
+    integer :: lev
+
+
+    if(dr_globalMe .eq. MASTER_PE) write(*,*) "[gr_amrexFinalize] Finalizing"
+  
+    ! NOTE: Arrays of multifabs use AMReX's 0-based level indexing scheme
+    do lev = 0, amrex_max_level
+        call gr_clearLevelCallback(lev)
+    end do
+
+    if (allocated(unk))            deallocate(unk)
+    if (allocated(gr_scratchCtr))  deallocate(gr_scratchCtr)
+    if (allocated(facevarx))       deallocate(facevarx)
+    if (allocated(facevary))       deallocate(facevary)
+    if (allocated(facevarz))       deallocate(facevarz)
+    if (allocated(fluxes))         deallocate(fluxes)
+    if (allocated(flux_registers)) deallocate(flux_registers)
+
+    call amrex_amrcore_finalize()
+    call amrex_octree_finalize()
+    call amrex_finalize()
+end subroutine gr_amrexFinalize
+
