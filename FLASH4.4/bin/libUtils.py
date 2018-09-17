98da3b83d8f4ef57739c395b622a96b66bc4134d
Austin Harris <jaharris87@users.noreply.github.com>
2018-09-17 15:14:35 -0400

    Create lib/name/object directory if it is missing

--- FLASH4.4/bin/libUtils.py
+++ FLASH4.4/bin/libUtils.py
@@ -114,6 +114,9 @@ class LibUnion(UserDict.UserDict):
      base = string.lower(lib)
      libDir = os.path.join(GVars.libDir,base)
      relLibDir = getRelPath(libDir,".")
+     objDir = os.path.join(libDir,'object')
+     if not os.path.isdir(objDir): os.mkdir(objDir)
+
      if not os.path.isdir(libDir): 
         return self.extLibFlags(lib,buildFlag) # args are of no use here
 
