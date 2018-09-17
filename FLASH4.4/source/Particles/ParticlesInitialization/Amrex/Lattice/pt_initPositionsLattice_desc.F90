c44ab1af8d6fa1033becb8961d176907ccf7c960
Saurabh Chawdhary <saurabh.chawdhary@gmail.com>
2018-09-17 10:11:53 -0500

    block based pt_initPositionsLattice_desc added for Amrex

--- FLASH4.4/source/Particles/ParticlesInitialization/Amrex/Lattice/pt_initPositionsLattice_desc.F90
+++ FLASH4.4/source/Particles/ParticlesInitialization/Amrex/Lattice/pt_initPositionsLattice_desc.F90
@@ -1,11 +1,11 @@
-!!****if* source/Particles/ParticlesInitialization/Lattice/pt_initPositionsLattice
+!!****if* source/Particles/ParticlesInitialization/Amrex/Lattice/pt_initPositionsLattice_desc
 !!
 !! NAME
-!!    pt_initPositionsLattice
+!!    pt_initPositionsLattice_desc
 !!
 !! SYNOPSIS
 !!
-!!    call pt_initPositionsLattice(integer(in)  :: blockID,
+!!    call pt_initPositionsLattice(integer(in)  :: block,
 !!                                 logical(OUT) :: success)
 !!
 !! DESCRIPTION
@@ -15,7 +15,7 @@
 !!
 !! ARGUMENTS
 !!
-!!  blockID:        local block ID containing particles to create
+!!  block:        local block (block_metadata_t) containing particles to create
 !!  success:        returns .TRUE. if positions for all particles
 !!                  that should be assigned to this block have been
 !!                  successfully initialized.
@@ -35,10 +35,11 @@
 !!***
 
 
-subroutine pt_initPositionsLattice (blockID,success)
+subroutine pt_initPositionsLattice_desc (block,success)
 
   use Particles_data, ONLY:  pt_numLocal, particles, pt_maxPerProc, &
        pt_xmin, pt_ymin, pt_zmin,pt_xmax, pt_ymax, pt_zmax,&
+       pt_containers, &
        pt_posAttrib,pt_velNumAttrib, pt_velAttrib,pt_typeInfo, pt_meshMe
   use Driver_interface, ONLY : Driver_abortFlash
        
@@ -50,13 +51,20 @@ subroutine pt_initPositionsLattice (blockID,success)
 
   use Grid_interface, ONLY : Grid_getBlkBoundBox, Grid_mapMeshToParticles
   use Logfile_interface, ONLY:  Logfile_stamp
+  use block_metadata,        ONLY : block_metadata_t
+  use amrex_particlecontainer_module, ONLY : amrex_particlecontainer, amrex_particle,&
+                                                            amrex_get_next_particle_id, amrex_get_cpu
+  use gr_physicalMultifabs, ONLY : unk
+  use amrex_multifab_module, ONLY : amrex_mfiter, &
+                                      amrex_mfiter_build, &
+                                      amrex_mfiter_destroy
 
   implicit none
 #include "constants.h"
 #include "Flash.h"
 #include "Particles.h"
 
-  integer, INTENT(in) :: blockID
+  type(block_metadata_t)    :: block
   logical,intent(OUT) :: success
 
   integer       :: i, j, k
@@ -70,6 +78,10 @@ subroutine pt_initPositionsLattice (blockID,success)
   real, dimension(MDIM) :: dxParticle = 0.0
   real, dimension(2,MDIM):: boundBox
   integer :: part_props=NPART_PROPS
+  type(amrex_particle) :: thisParticle
+  type(amrex_particlecontainer) :: thisPc
+  integer :: grd_index,tile_index,level_amrex
+  type(amrex_mfiter) :: mfi
 !----------------------------------------------------------------------
 
   !       Initialization now done in Particles_init.
@@ -80,7 +92,7 @@ subroutine pt_initPositionsLattice (blockID,success)
 
   ! Get grid geometry for this block 
 
-  call Grid_getBlkBoundBox(blockID,boundBox)
+  call Grid_getBlkBoundBox(block,boundBox)
   bxl = boundBox(LOW,IAXIS)
   bxu = boundBox(HIGH,IAXIS)
   
@@ -104,6 +116,15 @@ subroutine pt_initPositionsLattice (blockID,success)
   zpos = 0.0
   ypos = 0.0
   xpos = 0.0
+  !!Find mfi for amrex_particlecontainer routines
+  level_amrex = block%level-1
+  call amrex_mfiter_build(mfi, unk(level_amrex), tiling=.false.)
+    do while(mfi%next())
+        if((mfi%grid_index())==(block%grid_index)) then
+            print*,"mfi found with grid_index", mfi%grid_index()
+            exit
+        endif
+    end do
   
   loop_x:  do i = 1, pt_numX
      xpos = (i-0.5)*dxParticle(IAXIS) + pt_initialXMin
@@ -151,7 +172,7 @@ subroutine pt_initPositionsLattice (blockID,success)
               !! Check space allocation
               if (p > pt_maxPerProc) then
                  print *,' '
-                 print *,'Block', blockID,' on',pt_meshMe,' would get too many additional particles;'
+                 print *,'Block with grid_index', block%grid_index,' on',pt_meshMe,' would get too many additional particles;'
                  print *,'                 proc',pt_meshMe,' already had', pt_numLocal,' particles.'
                  print *,'PARAMETER pt_maxPerProc is set to ',pt_maxPerProc
                  call Logfile_stamp(pt_meshMe,&
@@ -163,14 +184,32 @@ subroutine pt_initPositionsLattice (blockID,success)
               !! particle is defined, set up data structure
               
               
-              particles(BLK_PART_PROP,p) = real(blockID)
-              particles(PROC_PART_PROP,p) = real(pt_meshMe)
-#ifdef MASS_PART_PROP
-              particles(MASS_PART_PROP,p) = 1.
-#endif
-              particles(POSX_PART_PROP,p) = xpos
-              particles(POSY_PART_PROP,p)  = ypos
-              particles(POSZ_PART_PROP,p)  = zpos
+!               particles(BLK_PART_PROP,p) = real(blockID)
+!               particles(PROC_PART_PROP,p) = real(pt_meshMe)
+! #ifdef MASS_PART_PROP
+!               particles(MASS_PART_PROP,p) = 1.
+! #endif
+!               particles(POSX_PART_PROP,p) = xpos
+!               particles(POSY_PART_PROP,p)  = ypos
+!               particles(POSZ_PART_PROP,p)  = zpos
+                thisPc=pt_containers(1)
+                thisParticle%pos(1) = xpos
+                if (NDIM > 1) then
+                    thisParticle%pos(2) = ypos
+                endif
+                if (NDIM == 3) then
+                    thisParticle%pos(3) = zpos
+                endif
+                thisParticle%vel = 0.d0
+                thisParticle%id  = amrex_get_next_particle_id()
+                thisParticle%cpu = amrex_get_cpu()  !DevNote :: or = pt_meshMe
+                grd_index=block%grid_index
+                !!DevNote Hard set tile index =0 for no tiling. This should come from block%a_new_field_for_tile_index
+                tile_index=0
+                print*,"pc size = ", size(pt_containers)
+                print*,"level, grid_index, tile_index: ",level_amrex,grd_index,tile_index
+!                 call pc%add_particle(lev, mfi, thisParticle)
+                call thisPc%add_particle(level_amrex, mfi, thisParticle)
               
               
            endif   !! end of IsInBlock .and. IsInSphere is true
@@ -178,7 +217,7 @@ subroutine pt_initPositionsLattice (blockID,success)
         enddo loop_z
      enddo loop_y
   enddo loop_x
-
+  call amrex_mfiter_destroy(mfi)
   !       Setting the particle database local number of particles
   pt_numLocal = p
   !       Now initialize velocity properties for the new particles
@@ -194,6 +233,6 @@ subroutine pt_initPositionsLattice (blockID,success)
 
 !----------------------------------------------------------------------
   
-end subroutine pt_initPositionsLattice
+end subroutine pt_initPositionsLattice_desc
 
 
