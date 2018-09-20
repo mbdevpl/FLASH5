de3a6ff944f5941ff9244b058062828311241bec
Austin Harris <jaharris87@users.noreply.github.com>
2018-09-20 10:32:25 -0400

    Revert "Revert "Merge branch 'setup_tweaks' into xnet_v7""
    This reverts commit 0e00e80f2ba80b674debcdba8c2962308a6f6fc9.

--- FLASH4.4/bin/libUtils.py
+++ FLASH4.4/bin/libUtils.py
@@ -114,6 +114,11 @@ class LibUnion(UserDict.UserDict):
      base = string.lower(lib)
      libDir = os.path.join(GVars.libDir,base)
      relLibDir = getRelPath(libDir,".")
+
+     if self[lib]["TYPE"]=="INTERNAL": # make sure object directory exists for internal lib
+        objDir = os.path.join(libDir,'object')
+        if not os.path.isdir(objDir): os.mkdir(objDir)
+
      if not os.path.isdir(libDir): 
         return self.extLibFlags(lib,buildFlag) # args are of no use here
 
