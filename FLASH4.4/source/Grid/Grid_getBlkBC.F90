a234cc628d68153a86a99edef34d594e9adf9429
adubey64 <adubey64@gmail.com>
2019-01-02 21:38:42 +0530

    got rid of functions with _desc extension

--- FLASH4.4/source/Grid/Grid_getBlkBC.F90
+++ FLASH4.4/source/Grid/Grid_getBlkBC.F90
@@ -1,72 +1,52 @@
-!!****f* source/Grid/Grid_getBlkBC
+!!****if* source/Grid/GridMain/Chombo/Grid_getBlkBC_desc
 !!
 !! NAME
-!!  Grid_getBlkBC
+!!  Grid_getBlkBC_desc
 !!
 !! SYNOPSIS
 !!
-!!  Grid_getBlkBC(integer(IN)  :: blockId,
+!!  call Grid_getBlkBC(block_metadata_t(IN)  :: blockDesc,
 !!                integer(OUT) :: faces(2,MDIM),
 !!       optional,integer(OUT) :: onBoundary(2,MDIM))
 !!                    
 !! DESCRIPTION 
 !!  Returns the boundary condition for each face of the block.
-!!
-!!  This function finds out if a block face is
+!!  
+!!  this function finds out if a block face is
 !!  on the physical boundary, and if so, returns the boundary condition.
-!!
-!!  This function finds out if a block face is on the physical
-!!  boundary. If the face is on the boundary and the boundary
-!!  conditions is not periodic, it returns the boundary condition in
-!!  array "faces". If the argument "onBoundary" is present then if
-!!  all boundary conditions are not periodic, it returns all values
-!!  identical to faces. If one or more boundary conditions are
-!!  periodic, then the array "faces" returns NOT_BOUNDARY in corresponding elements,
-!!  while the array "onBoundary" returns the value
-!!  PERIODIC on corresponding faces.
-!!
 !!  The boundary conditions are defined in the header file
 !!  constants.h, ie. OUTFLOW, REFLECTING, PERIODIC.  NOT_BOUNDARY, also
-!!  defined in constants.h, is returned if a block face is not on a 
+!!  defined in constants.h is returned if a block face is not on a 
 !!  physical boundary.
 !!   
 !! ARGUMENTS 
 !!
-!!  blockId - the local blockId 
-!!  faces   - array returned holding boundary conditions, except when periodic,
-!!            if any of the faces of the block are on a physical boundary. 
+!!  blockDesc - descriptor for local block
+!!  faces   - array returned holding boundary conditions
 !!                
-!!            The first index of the array can take on values LOW or
-!!            HIGH, and the second index can be IAXIS, JAXIS or KAXIS.
+!!            the first index of the array can take on values LOW or
+!!            HIGH, and the second index can be IAXIS, JAXIS or KAXIS
 !! onBoundary - array returned with boundary conditions including periodic
 !!
 !! NOTES
 !!
 !!   The #define constants LOW, HIGH, IAXIS, JAXIS and KAXIS
 !!   are defined in constants.h and are
-!!   meant to ease the readability of the code.
+!!   meant to ease the readability of the code.       
 !!   instead of faces(2,3) = PERIODIC the code reads
-!!   faces(HIGH, KAXIS) = PERIODIC.
+!!   faces(HIGH, KAXIS) = PERIODIC
 !!
 !!***
 
-
-subroutine Grid_getBlkBC(block, faces,onBoundary)
-  use block_metadata, ONLY : block_metadata_t
-
-  implicit none
 #include "constants.h"
-  
-  type(block_metadata_t), intent(in) :: block
+#include "Flash.h"
+
+subroutine Grid_getBlkBC(blockDesc, faces, onBoundary)
+  use Grid_data, ONLY : gr_globalDomain, gr_domainBC
+  use Grid_interface, ONLY : Grid_getBlkBoundBox
+  use block_metadata, ONLY : block_metadata_t
+  implicit none
+  type(block_metadata_t), intent(in) :: blockDesc
   integer, dimension(2,MDIM),intent(out):: faces
   integer, optional, dimension(LOW:HIGH,MDIM),intent(out):: onBoundary
-
-  faces = NOT_BOUNDARY
-  
-  if (present (onBoundary)) then     
-     onBoundary = NOT_BOUNDARY
-  endif
- 
-
-  return
 end subroutine Grid_getBlkBC
