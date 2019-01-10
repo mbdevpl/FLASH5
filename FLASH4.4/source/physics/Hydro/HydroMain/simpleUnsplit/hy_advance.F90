d6b511462f063cb0cd24bf9c693d92ea15d355ea
Jared O'Neal <joneal@anl.gov>
2019-01-10 15:19:31 -0600

    (Issue #320) Temporary hack that gets tiling working with simpleUnsplit.  The issue is that Uin and Uout point to the same FAB in the current multifab. Therefore, with tiling, writing to Uout overwrites the Uin data from the previous timestep.  Therefore, other tiles in the same block might update their interior data using data from the current timestep.  These changes will have to be done as they are low-level and AMReX specific.

--- FLASH4.4/source/physics/Hydro/HydroMain/simpleUnsplit/hy_advance.F90
+++ FLASH4.4/source/physics/Hydro/HydroMain/simpleUnsplit/hy_advance.F90
@@ -13,18 +13,26 @@
 
 subroutine hy_advance(simTime, dt, dtOld)
   use Grid_interface,   ONLY : Grid_getTileIterator, &
-                               Grid_releaseTileIterator
+                               Grid_releaseTileIterator, &
+                               Grid_getMaxRefinement
   use Timers_interface, ONLY : Timers_start, &
                                Timers_stop
   use hy_interface,     ONLY : hy_advanceBlk
   use flash_iterator,   ONLY : flash_iterator_t
   use flash_tile,       ONLY : flash_tile_t
 
+  use gr_physicalMultifabs,  ONLY : unk
+  use amrex_multifab_module, ONLY : amrex_multifab, &
+                                    amrex_multifab_build, &
+                                    amrex_multifab_destroy
+
   implicit none
 
   real, intent(IN) :: simTime
   real, intent(IN) :: dt
   real, intent(IN) :: dtOld
+ 
+  type(amrex_multifab) :: tmp
 
   real, pointer :: Uout(:,:,:,:)
   real, pointer :: Uin(:,:,:,:)
@@ -32,22 +40,42 @@ subroutine hy_advance(simTime, dt, dtOld)
   type(flash_iterator_t) :: itor
   type(flash_tile_t)     :: tileDesc
 
-  call Grid_getTileIterator(itor, LEAF, tiling=.FALSE.)
+  integer :: level
+  integer :: finest_level
+
+  nullify(Uin)
+  nullify(Uout)
+
   call Timers_stop("loop1")
-  do while(itor%isValid())
-     call itor%currentTile(tileDesc)
+  call Grid_getMaxRefinement(finest_level, mode=3)
+  do level = 1, finest_level
+     call amrex_multifab_build(tmp, &
+                               unk(level-1)%ba, &
+                               unk(level-1)%dm, &
+                               NUNK_VARS, NGUARD)
 
-     call tileDesc%getDataPtr(Uout, CENTER)
-     Uin => Uout
+     call Grid_getTileIterator(itor, LEAF, level=level, tiling=.TRUE.)
+     do while(itor%isValid())
+        call itor%currentTile(tileDesc)
 
-     call hy_advanceBlk(tileDesc, Uin, Uout, simTime, dt, dtOld, SWEEP_ALL)
+        call tileDesc%getDataPtr(Uin, CENTER)
+        associate(lo => lbound(Uin))
+           Uout(lo(1):, lo(2):, lo(3):, lo(4):) => tmp%dataPtr(tileDesc%grid_index)
+        end associate
 
-     call tileDesc%releaseDataPtr(Uout, CENTER)
- 
-     call itor%next()
+        call hy_advanceBlk(tileDesc, Uin, Uout, simTime, dt, dtOld, SWEEP_ALL)
+
+        call tileDesc%releaseDataPtr(Uin, CENTER)
+        nullify(Uout)
+
+        call itor%next()
+     end do
+     call Grid_releaseTileIterator(itor)
+
+     call unk(level-1)%copy(tmp, UNK_VARS_BEGIN, UNK_VARS_BEGIN, NUNK_VARS, 0)
+     call amrex_multifab_destroy(tmp)
   end do
   call Timers_stop("loop1")
-  call Grid_releaseTileIterator(itor)
 
 #ifdef DEBUG_DRIVER
   print*, 'return from Hydro/MHD timestep'  ! DEBUG
