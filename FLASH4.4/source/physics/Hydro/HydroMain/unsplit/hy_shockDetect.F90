9c4284c30ecc4aa5553feac0ddd3d1ae3c9e110c
Klaus Weide <klaus@flash.uchicago.edu>
2019-03-19 21:35:14 -0500

    (Issue #441) restore hy_shockDetect to working order
    Updated to use new Grid_tile etc. convention.
    
    This seems to do the job, somewhat tested with in-progress 3D
    DustCollapse setup.
    
    Note that the hy_shockDetectBlk interface is modified.

--- FLASH4.4/source/physics/Hydro/HydroMain/unsplit/hy_shockDetect.F90
+++ FLASH4.4/source/physics/Hydro/HydroMain/unsplit/hy_shockDetect.F90
@@ -1,71 +1,49 @@
 #include "constants.h"
 
 subroutine hy_shockDetect
-!  use Grid_interface, ONLY : Grid_getDeltas, &
-!                             Grid_getBlkPtr, Grid_releaseBlkPtr, &
-!                             Grid_getLeafIterator, Grid_releaseLeafIterator
+  use Grid_interface, ONLY : Grid_getTileIterator, Grid_releaseTileIterator
   use hy_interface,   ONLY : hy_shockDetectBlk
   use Grid_iterator,  ONLY : Grid_iterator_t
   use Grid_tile,      ONLY : Grid_tile_t
-  use Hydro_data, ONLY : hy_fluxCorrect,      &
-                         hy_gref,             &
-                         hy_useGravity,       &
-                         hy_units,            &
-                         hy_gcMaskSize,       &
-                         hy_gcMask,           &
-                         hy_unsplitEosMode,   &
-                         hy_eosModeGc,        &
-                         hy_eosModeAfter,     &
-                         hy_updateHydroFluxes,&
-                         hy_geometry,         &
-                         hy_fluxCorVars,      &
-                         hy_cfl,              &
-                         hy_cfl_original,     &
-                         hy_numXN,            &
-                         hy_fullRiemannStateArrays,    &
-                         hy_fullSpecMsFluxHandling,   &
-                         hy_dtmin,            &
-                         hy_simTime,          &
-                         hy_simGeneration,    &
-                         hy_shockDetectOn
+  use Hydro_data, ONLY : hy_shockDetectOn
   use Driver_interface, ONLY : Driver_abortFlash
 
   implicit none
 
 #include "UHD.h"
 
-!  real, dimension(MDIM) :: del
+  type(Grid_iterator_t) :: itor
+  type(Grid_tile_t)     :: tileDesc
 !
-!  type(Grid_iterator_t) :: itor
-!  type(Grid_tile_t)     :: tileDesc
+  integer, dimension(LOW:HIGH,MDIM) :: limits,grownLimits
+
+  real, dimension(MDIM) :: del
 !
-!  integer, dimension(LOW:HIGH,MDIM) :: blkLimits,blkLimitsGC
-!  real, dimension(:,:,:,:),pointer :: Uin
-!  real,dimension(:,:,:,:), pointer :: Uout
+  real, dimension(:,:,:,:),pointer :: Uin
+  real,dimension(:,:,:,:), pointer :: Uout
 
-  call Driver_abortFlash("[hy_shockDetect] Implement for use with tiling")
-
-!  call Grid_getLeafIterator(itor)
-!  do while(itor%is_valid())
-!     call itor%blkMetaData(blockDesc)
+  call Grid_getTileIterator(itor,LEAF)
+  do while(itor%isValid())
+     call itor%currentTile(tileDesc)
 !     
-!     blkLimits(:,:)   = blockDesc%localLimits
-!     blkLimitsGC(:,:) = blockDesc%localLimitsGC
+     limits(:,:)   = tileDesc%limits
+     grownLimits(:,:) = tileDesc%grownLimits
 !     
-!     call Grid_getBlkPtr(blockDesc, Uout,localFlag=.TRUE.)
-!     Uin => Uout
+     call tileDesc % getDataPtr(Uout,CENTER,localFlag=.FALSE.)
+     Uin => Uout
 !     
-!     !! Detect shocks
-!     if (hy_shockDetectOn) then
-!        call Grid_getDeltas(blockDesc%level,del)
+     !! Detect shocks
+     if (hy_shockDetectOn) then
+        call tileDesc % deltas(del)
+        call hy_shockDetectBlk(Uin,lbound(Uin),grownLimits,Uout,lbound(Uout),limits,del)
 !        call hy_shockDetectBlk(Uin,blkLimitsGC,Uout,blkLimits,del)
-!     end if
+     end if
 !     
 !     
-!     call Grid_releaseBlkPtr(blockDesc, Uout)
+     call tileDesc % releaseDataPtr(Uout,CENTER)
 !     
-!     call itor%next()
-!  end do
-!  call Grid_releaseLeafIterator(itor)
+     call itor%next()
+  end do
+  call Grid_releaseTileIterator(itor)
   
 end subroutine hy_shockDetect
