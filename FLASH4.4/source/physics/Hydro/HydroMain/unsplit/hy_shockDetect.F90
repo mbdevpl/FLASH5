e33d998433b5c60ffb9799b70ba2ab31e7516afe
Klaus Weide <klaus@flash.uchicago.edu>
2019-03-20 10:23:59 -0500

    some cleanup in hy_shockDetect
    Got rid of explicit localFlag actual argument. Ordered some lines.

--- FLASH4.4/source/physics/Hydro/HydroMain/unsplit/hy_shockDetect.F90
+++ FLASH4.4/source/physics/Hydro/HydroMain/unsplit/hy_shockDetect.F90
@@ -1,12 +1,12 @@
 #include "constants.h"
 
 subroutine hy_shockDetect
-  use Grid_interface, ONLY : Grid_getTileIterator, Grid_releaseTileIterator
-  use hy_interface,   ONLY : hy_shockDetectBlk
+  use Driver_interface, ONLY : Driver_abortFlash
   use Grid_iterator,  ONLY : Grid_iterator_t
   use Grid_tile,      ONLY : Grid_tile_t
+  use Grid_interface, ONLY : Grid_getTileIterator, Grid_releaseTileIterator
   use Hydro_data, ONLY : hy_shockDetectOn
-  use Driver_interface, ONLY : Driver_abortFlash
+  use hy_interface,   ONLY : hy_shockDetectBlk
 
   implicit none
 
@@ -14,34 +14,33 @@ subroutine hy_shockDetect
 
   type(Grid_iterator_t) :: itor
   type(Grid_tile_t)     :: tileDesc
-!
+
   integer, dimension(LOW:HIGH,MDIM) :: limits,grownLimits
 
   real, dimension(MDIM) :: del
-!
+
   real, dimension(:,:,:,:),pointer :: Uin
   real,dimension(:,:,:,:), pointer :: Uout
 
   call Grid_getTileIterator(itor,LEAF)
   do while(itor%isValid())
      call itor%currentTile(tileDesc)
-!     
+
      limits(:,:)   = tileDesc%limits
      grownLimits(:,:) = tileDesc%grownLimits
-!     
-     call tileDesc % getDataPtr(Uout,CENTER,localFlag=.FALSE.)
+
+     call tileDesc % getDataPtr(Uout,CENTER)
      Uin => Uout
-!     
+
      !! Detect shocks
      if (hy_shockDetectOn) then
         call tileDesc % deltas(del)
         call hy_shockDetectBlk(Uin,lbound(Uin),grownLimits,Uout,lbound(Uout),limits,del)
-!        call hy_shockDetectBlk(Uin,blkLimitsGC,Uout,blkLimits,del)
      end if
-!     
-!     
+
+
      call tileDesc % releaseDataPtr(Uout,CENTER)
-!     
+
      call itor%next()
   end do
   call Grid_releaseTileIterator(itor)
